6a1ab056356cebdbdc7fc299ae3a30dde353f526
Mateusz Pustelnik <mateusz.pustelnik13@gmail.com>
2025-06-23 21:25:32 +0200

    configured alembic, postgreSQL in docker with docker-compose and added post endpoint for users table

---
+++ backend/app/models/user.py
@@ -0,0 +1,15 @@
+import uuid
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    addiction_type = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
